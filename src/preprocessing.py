c3ad75b9b3072710916e721702f1ecb62ed6d924
Pyt-git <weicky0804@gmail.com>
2026-06-12 13:21:38 -0400

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,3 +5,15 @@ def clean_text(text):
     text = re.sub(r'\s+', ' ', text)
     text = text.strip()
     return text
+
+label_map = {
+    "positive": 1, 
+    "neutral": 0, 
+    "negative": -1
+}
+
+def add_sample(df, text, label): 
+    clean = clean_text(text)
+    label_id = label_map[label]
+    df.loc[len(df)] = [text, clean, label, label_id]
+    return df
